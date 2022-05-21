b6469cc13d8a455fa410a9bc5e4485fe01b54102
SoU <poly30@naver.com>
2022-05-22 08:46:40 +0900

    위치 검색 기능 개발

--- app/src/main/java/com/example/convenience/MainActivity.java
+++ app/src/main/java/com/example/convenience/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.convenience;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,4 +31,17 @@ public class MainActivity extends AppCompatActivity {
         Intent ACT_Sale = new Intent(this,SaleActivity.class);
         startActivity(ACT_Sale);
     }
+
+    public void onMapsClicked(View v)
+    {
+        Toast.makeText(this, "위치 정보 화면",Toast.LENGTH_LONG).show();
+
+        Intent ACT_Maps = new Intent(this,MapsActivity.class);
+        startActivity(ACT_Maps);
+    }
+
+
+
+
+
 }
